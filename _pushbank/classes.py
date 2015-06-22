014bf9e8629c8e7fa42cf35e9d8b7ad5eebb838b
ssut <ssut@ssut.me>
2015-06-22 18:55:12 +0900

    수정: 더 다양한 형식으로 제목을 맺을 수 있도록 수정

--- _pushbank/classes.py
+++ _pushbank/classes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Plugin:
     wait = False
 
@@ -7,4 +9,29 @@ class Plugin:
     @property
     def options(self):
         return self._options
+
+    def format_args(self, account, history):
+        transaction_type = '출금' if history.pay > 0 else '입금'
+        value = history.withdraw - history.pay
+        value_readable = '{:,}'.format(value)
+        absvalue = abs(value)
+        absvalue_readable = '{:,}'.format(absvalue)
+        d = {
+            'name': account.account,
+            'account_balance': account.balance,
+            'balance': history.balance,
+            'date': history.date,
+            'type': history.type,
+            'depositor': history.depositor,
+            'pay': history.pay,
+            'withdraw': history.withdraw,
+            'transaction_type': transaction_type,
+            'value': value,
+            'value_readable': value_readable,
+            'absvalue': absvalue,
+            'absvalue_readable': absvalue_readable,
+            'distributor': history.distributor,
+            'now': datetime.now(),
+        }
+        return d
 
